ecbb3cd76b6fc62e2af2149f99905d4ae2704f68
LELOUCH VI BRITANNIA <bvterrell.pay@icloud.com>
2024-03-05 09:59:52 +0700

    KONTOLE ARYA MAMBU YOGA

--- app/page.tsx
+++ app/page.tsx
@@ -16,13 +16,7 @@ export default function Home() {
 				<h1 className={title()}>Start mine&nbsp;</h1>
 				<h1 className={title({ color: "violet" })}>Univer&nbsp;</h1>
 				<br />
-				<h2 className={subtitle({ class: "mt-4" })}>
-					Join Beautiful, fast and modern blockchain.
-				</h2>
-			</div>
-
-
-			<div className="mt-4">
+				<div className="mt-4">
 				<Snippet hideSymbol hideCopyButton variant="flat">
 					<span>
 						<Code className="mb-5" color="success">currenly all worker is online</Code>
@@ -36,6 +30,26 @@ export default function Home() {
 					</span>
 				</Snippet>
 			</div>
+				<h2 className={subtitle({ class: "mt-4" })}>
+					Join Beautiful, fast and modern blockchain.
+				</h2>
+			</div>
+
+
+			{/* <div className="mt-4">
+				<Snippet hideSymbol hideCopyButton variant="flat">
+					<span>
+						<Code className="mb-5" color="success">currenly all worker is online</Code>
+						<BodyCounter />
+						<div className="flex justify-center mt-5 mb-5">
+							<Button color="success" variant="ghost">
+								MINER ON!
+							</Button> 
+						</div>
+						
+					</span>
+				</Snippet>
+			</div> */}
 		</section>
 	);
 }
