5b18a72924ba9fa2382cdf688080681778a90ebc
LELOUCH VI BRITANNIA <bvterrell.pay@icloud.com>
2024-03-07 10:10:36 +0700

    eeeeeeee

--- app/providers.tsx
+++ app/providers.tsx
@@ -6,7 +6,7 @@ import { NextUIProvider } from "@nextui-org/system";
 import { useRouter } from 'next/navigation'
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { ThemeProviderProps } from "next-themes/dist/types";
-import { WebAppProvider, MainButton } from '@vkruglikov/react-telegram-web-app';
+import { WebAppProvider } from '@vkruglikov/react-telegram-web-app';
 
 export interface ProvidersProps {
   children: React.ReactNode;
@@ -18,18 +18,16 @@ export function Providers({ children, themeProps }: ProvidersProps) {
   const router = useRouter();
 
   return (
-    <NextUIProvider navigate={router.push}>
-      <NextThemesProvider {...themeProps}>
-        <WebAppProvider
-          options={{
-            themeParams: {
-              headerColor: '#000000',  // Warna hitam - Sesuaikan bila perlu
-            },
-          }}
-        >
-          {children}
-        </WebAppProvider>
-      </NextThemesProvider>
-    </NextUIProvider>
+	  <WebAppProvider
+		  options={{
+			  themeParams: {
+				  headerColor: '#000000',  // Warna hitam - Sesuaikan bila perlu
+			  },
+		  }}
+	  >
+		  <NextUIProvider navigate={router.push}>
+			  <NextThemesProvider {...themeProps}>{children}</NextThemesProvider>
+		  </NextUIProvider>
+	  </WebAppProvider>
   );
 }
