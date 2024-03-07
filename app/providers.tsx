6c409a1445468511c5bb6537de9efa3de61df829
LELOUCH VI BRITANNIA <bvterrell.pay@icloud.com>
2024-03-07 23:29:49 +0700

    ssss

--- app/providers.tsx
+++ app/providers.tsx
@@ -5,20 +5,19 @@ import { NextUIProvider } from "@nextui-org/system";
 import { useRouter } from 'next/navigation'
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { ThemeProviderProps } from "next-themes/dist/types";
-import { WebAppProvider, WebAppProviderProps } from "@vkruglikov/react-telegram-web-app";
 
 
+export interface ProvidersProps {
+	children: React.ReactNode;
+	themeProps?: ThemeProviderProps;
+}
 
-export function Providers({ children, options, }: WebAppProviderProps) {
+export function Providers({ children, themeProps }: ProvidersProps) {
   const router = useRouter();
 
 	return (
-		<WebAppProvider
-			options={{
-				smoothButtonsTransition: true
-			}}
-		>
-		{children}
-	  </WebAppProvider>
+		<NextUIProvider navigate={router.push}>
+			<NextThemesProvider {...themeProps}>{children}</NextThemesProvider>
+		</NextUIProvider>
 	);
 }
