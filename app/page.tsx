aac7710161e3dc8f7853239b46d57fba4bf0bf66
LELOUCH VI BRITANNIA <bvterrell.pay@icloud.com>
2024-03-08 11:42:09 +0700

    KEREN

--- app/page.tsx
+++ app/page.tsx
@@ -13,36 +13,40 @@ export default function Home() {
 				<h1 className={title()}>Start mine&nbsp;</h1>
 				<h1 className={title({ color: "violet" })}>Univermine&nbsp;</h1>
 				<br />
+				<h1 className={title()}>Start mine&nbsp;</h1>
+				<h1 className={title({ color: "violet" })}>Univermine&nbsp;</h1>
+				<br />
+				<h1 className={title()}>Start mine&nbsp;</h1>
+				<h1 className={title({ color: "violet" })}>Univermine&nbsp;</h1>
 				<div className="mt-4">
-				<Snippet hideSymbol hideCopyButton variant="flat">
-					<span>
-						<Code className="mb-5" color="success">currenly all worker is online</Code>
-						<BodyCounter />
-						<div className="flex justify-center mt-5 mb-5">
-							<Button color="success" variant="ghost">
-								MINER ON!
-							</Button> 
-							
-						</div>
+					<Snippet hideSymbol hideCopyButton variant="flat">
+						<span>
+							<Code className="mb-5" color="success">currenly all worker is online</Code>
+							<BodyCounter />
+							<div className="flex justify-center mt-5 mb-5">
+								<Button color="success" variant="ghost">
+									MINER ON!
+								</Button>
+
+							</div>
+
+						</span>
+					</Snippet>
+					<div className="mt-2">
 						
-					</span>
-				</Snippet>
-			</div>
-				<h2 className={subtitle({ class: "mt-4" })}>
-					Join Beautiful, fast and modern blockchain.
-				</h2>
-				<h2 className={subtitle({ class: "mt-4" })}>
-					Join Beautiful, fast and modern blockchain.
-				</h2>
-				<h2 className={subtitle({ class: "mt-4" })}>
-					Join Beautiful, fast and modern blockchain.
-				</h2>
-				<h2 className={subtitle({ class: "mt-4" })}>
-					Join Beautiful, fast and modern blockchain.
-				</h2>
-				<h2 className={subtitle({ class: "mt-4" })}>
-					Join Beautiful, fast and modern blockchain.
-				</h2>
+						<h1 className={title()}>Start mine&nbsp;</h1>
+						<h1 className={title({ color: "violet" })}>Univermine&nbsp;</h1>
+						<br />
+						<h1 className={title()}>Start mine&nbsp;</h1>
+						<h1 className={title({ color: "violet" })}>Univermine&nbsp;</h1>
+						<br />
+						<h1 className={title()}>Start mine&nbsp;</h1>
+						<h1 className={title({ color: "violet" })}>Univermine&nbsp;</h1>
+						<br />
+						<h1 className={title()}>Start mine&nbsp;</h1>
+						<h1 className={title({ color: "violet" })}>Univermine&nbsp;</h1>
+					</div>
+				</div>
 			</div>
 		</section>
 	);
